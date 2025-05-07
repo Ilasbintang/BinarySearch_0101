66ab307d5a082c419c8dee9742ed2048b0eca12a
Ilasbintang <ilasbintang727@gmail.com>
2025-05-07 15:41:39 +0700

    menyelesaikan binary search

--- tugas.cpp
+++ tugas.cpp
@@ -73,3 +73,20 @@ void binarySearch()
         
         int low = 0;
         int high = nPanjang - 1;
+        do
+        {
+            int mid = (low + high)/2;
+            if (element[mid] == x)
+            {
+                cout << "\n[√] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x < element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
