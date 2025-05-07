bcfc7a50ece2432823b9991d2dc3214131e69636
Ilasbintang <ilasbintang727@gmail.com>
2025-05-07 15:36:06 +0700

    Membuat perintah data ke

--- tugas.cpp
+++ tugas.cpp
@@ -19,3 +19,12 @@ void input()
             cout << "\n{!} Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+    cout << "\n========================================\n";
+    cout << "          Maukkan Elemen Array          \n";
+    cout << "\n======================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
